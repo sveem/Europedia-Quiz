46dc307b9481c67c10326d8eb9ca0c803e369ef6
Svetlin Mladenov <svem@gmx.com>
2017-01-27 15:19:06 -0500

    modified package.json, render simple text

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-
-             
+import React from 'react';
+import ReactDOM from 'react-dom';            
     
+const App = function() {
+  return <div>EUROPEDIA QUIZ!</div>;
+};
+
+
+ReactDOM.render(App);
